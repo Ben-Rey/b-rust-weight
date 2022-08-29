5e5494a78c67a91182bd119deb7101e12f1a4c34
fen|x <benjamin.reynes.dev@gmail.com>
2022-08-29 09:03:08 +0200

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
+    let mars_weight:f32 = calculate_weight_on_mars(46.0);
+
+    println!("Weight on Mars: {}Kg", mars_weight);
+}
+
+
+fn calculate_weight_on_mars(_weight: f32) -> f32 {
+    (_weight / 9.81) * 3.711
 }
