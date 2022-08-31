c637d07e83bcdeb20cbe58628a5b5eff0d8add87
fen|x <benjamin.reynes.dev@gmail.com>
2022-08-31 21:37:04 +0200

    end

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight in Kg");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    
+    io::stdin().read_line(&mut input).unwrap();
+    
+    // To remove white spaces and unwrap because parse return a Result or error
+    let weight:f32 = input.trim().parse().unwrap();
 
-    let mars_weight:f32 = calculate_weight_on_mars(46.0);
-
+    let mars_weight:f32 = calculate_weight_on_mars(weight);
+    
     println!("Weight on Mars: {}Kg", mars_weight);
 }
 
